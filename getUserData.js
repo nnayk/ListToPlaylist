614637bff8714225c3ec8cbedf7ae9b1b8a2d04e
nnayk <79224511+nnayk@users.noreply.github.com>
2022-07-31 16:35:00 -0700

    able to add some songs from a text file to a new playlist

--- getUserData.js
+++ getUserData.js
@@ -1,16 +1,16 @@
 // const fetch = require("node-fetch")
-import SpotifyWebApi from "spotify-web-api-node"
+const SpotifyWebApi = require("spotify-web-api-node")
 const access_token = 'BQDpa39i9YbiAQSFrcsG1GR_x2zVc0xoAEVpjqbEhCOxlhgujMojm8V1MwX7274k9HFlYskqM7pEhhcXDTzGyjzkHlfASPzLSH0tTaLWIFFawoj-R6Jeqy6HOvoOqiE4uuxqNHmYAe6F8E-krAkEjbSTsfQexueOkzLV1ZOj4t3Lsk6u4bUkIbb3tEiXOqgCVeG2Usstn8jPUGcKjw4mfOe52HmctehnFfsRzbGB8L655g4OEEKi4VnxTkPGWxx_WgP0Bd-r1g2oMorhgStbwfgc3pUcpmuvGYMwk3kuMwCnx7dfFf2z6XcPYmv5-dqO6etttq-kVpOTCeH0m-k7'
 const refresh_token = 'AQB8jqdlNBDxnKrUviefUcynBvudyJkhKAvUM4CxyLL_UEaPSyA4mtusyciIgMa1SNxXhju-a9VrR_K32Imz6aRfZFpHWXdfV_sy5QSqAAkXS67Pc6xACwhoLxA8OyPktQw'
 const spotifyApi = new SpotifyWebApi();
 spotifyApi.setAccessToken(access_token);
 
-(async () => {
-  const me = await spotifyApi.getMe();
-  console.log(me);
-})().catch(e => {
-  console.error(e);
-});
+// (async () => {
+//   const me = await spotifyApi.getMe();
+//   console.log(me);
+// })().catch(e => {
+//   console.error(e);
+// });
 
 async function getPlayLists()
 {
@@ -57,18 +57,29 @@ async function createPlaylist()
 //     console.log(jsOn)
 // }
 
-async function searcher(artist,song)
+async function searcher(song,artist)
 {
-  // return spotifyApi.searchTracks(`track:${song} artist:${artist}`)
-    spotifyApi.searchTracks(`track:Black or White artist:Michael Jacksons`)
-  .then(function(data) {
-    console.log(data.body)
-    console.log(data.body.items)
-  }, function(err) {
-    console.log('Something went wrong!', err);
-    return false
-  });
+  return spotifyApi.searchTracks(`track:${song} artist:${artist}`)
+  //   spotifyApi.searchTracks(`track:${song} artist:${artist}`)
+  // .then(function(data) {
+  //   console.log(JSON.stringify(data.body.tracks.items[0].uri))
+  //   return data
+  // }, function(err) {
+  //   console.log('Something went wrong!', err);
+  //   return false
+  // });
 }
+
+// async function searcher(song,artist)
+// {
+//     spotifyApi.searchTracks(`track:${song} artist:${artist}`)
+//   .then(function(data) {
+//     console.log('Search tracks by "Alright" in the track name and "Kendrick Lamar" in the artist name', JSON.stringify(data.body.tracks.items[0].uri));
+//   }, function(err) {
+//     console.log('Something went wrong!', err);
+//   });
+
+// }
 
 
 async function billieJean(song,artist)
@@ -83,23 +94,33 @@ async function billieJean(song,artist)
 }
 
 
-// searcher("Black or White","Michael Jackson")
+async function getData()
+{
+let title = await searcher("Black or White","Michael Jackson")
+console.log("TITLE",JSON.stringify(title.body.tracks.items[0].uri))
 
-// async function addTracks(tracks)
-// {
-// spotifyApi.addTracksToPlaylist('2q4ibk2cVymxYX8iZzyY8J', tracks)
-//   .then(function(data) {
-//     console.log('Added tracks to playlist!');
-//   }, function(err) {
-//     console.log('Something went wrong!', err);
-//     return false
-//   });
-// }
+// title = billieJean("Black or White","Michael Jackson")
+}
+
+// getData()
+// spotifyApi.searchTracks(`track:Black or White artist:Michael Jackson`).then(function(data){console.log(JSON.stringify(data.body))});
+
+
+async function addTracks(tracks)
+{
+spotifyApi.addTracksToPlaylist('2q4ibk2cVymxYX8iZzyY8J', [tracks])
+  .then(function(data) {
+    console.log('Added tracks to playlist!');
+  }, function(err) {
+    console.log('Something went wrong!', err);
+    return false
+  });
+}
 
 // getPlayLists()
-billieJean("Black or White","Michael Jackson")
+
 // createPlaylist()
 // addTracks()
 // getTrack()
 
-// module.exports = {addTracks,searcher}
+module.exports = {addTracks,searcher}
--- testFileRead.js
+++ testFileRead.js
@@ -1,58 +1,62 @@
-// const fs = require('fs')
-// const readline = require('readline')
-// const getUser = require("./getUserData")
+const fs = require('fs')
+const readline = require('readline')
+const getUser = require("./getUserData")
 
-// // async function readF()
-// // {
-// // const file = await fs.readFile('./songs.pdf',{encoding:'utf8'})
-// // console.log(file)
-// // }
-
-// // const readline = require('readline');
-
-// async function processLineByLine() {
-//   const fileStream = fs.createReadStream('songs.pdf');
-
-//   const rl = readline.createInterface({
-//     input: fileStream,
-//     crlfDelay: Infinity
-//   });
-//   // Note: we use the crlfDelay option to recognize all instances of CR LF
-//   // ('\r\n') in input.txt as a single line break.
-
-//   let currArtist = ""
-//   let song = ""
-// let tracks = []
-
-//   for await (const line of rl) {
-//     // Each line in input.txt will be successively available here as `line`.
-//     console.log(line,line.length)
-//     if (line.length > 0)
-//     {
-//         if (line.includes(':') || line.includes('--'))
-//         {
-//             currArtist = line.slice(0,-1)
-//         }
-//         else
-//         {
-//             song = line
-//             const songInfo = await getUser.searcher(currArtist,song)
-//             console.log(songInfo)
-//             if (songInfo != undefined)
-//             {
-//             tracks.push(songInfo.body.tracks.items[0]['uri'])
-//             }
-//             // console.log(`${song} details:\n ${songInfo.body.tracks.items[0]['uri']}`)
-
-//         }
-//     }
-//   }
-//   console.log("TRACKS",tracks)
-//   getUser.addTracks(tracks)
+// async function readF()
+// {
+// const file = await fs.readFile('./songs.pdf',{encoding:'utf8'})
+// console.log(file)
 // }
 
-// processLineByLine();
+// const readline = require('readline');
 
-// // getUser.searcher()
+async function processLineByLine() {
+  const fileStream = fs.createReadStream('songs.pdf');
 
-// // readF()
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+  // Note: we use the crlfDelay option to recognize all instances of CR LF
+  // ('\r\n') in input.txt as a single line break.
+
+  let currArtist = ""
+  let song = ""
+let tracks = []
+
+  for await (const line of rl) {
+    // Each line in input.txt will be successively available here as `line`.
+    console.log(line,line.length)
+    if (line.length > 0)
+    {
+        if (line.includes(':') || line.includes('--'))
+        {
+            currArtist = line.slice(0,-1)
+        }
+        else
+        {
+            song = line
+            // let title = await searcher("Black or White","Michael Jackson")
+            // console.log("TITLE",JSON.stringify(title.body.tracks.items[0].uri)) 
+            let songInfo = await getUser.searcher(song,currArtist)
+            try
+            {
+            console.log(`${song} details:\n ${songInfo.body.tracks.items[0]['uri']}`)
+            getUser.addTracks(songInfo.body.tracks.items[0]['uri'])
+            } catch (e) 
+            {
+              console.log("ERROR ADDING TRACK")
+            }
+
+        }
+    }
+  }
+  console.log("TRACKS",tracks)
+  // getUser.addTracks(tracks)
+}
+
+processLineByLine();
+
+// getUser.searcher()
+
+// readF()
